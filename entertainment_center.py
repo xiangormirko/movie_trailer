5bd42de0b8206e1c2a34c83c045985f62a0df696
xiangormirko <xiangormirko@gmail.com>
2015-05-20 09:53:48 +0200

    revision after comments 1

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
+# filename: entertainment_center.py
+# description: Part of the movie trailer website project. This script creates instances of class Movie and TvShows and calls fresh_tomatoes.py to generate website
+# author: Xiang Zhao
+
+
+#imports media.py which contains class specificaitons. imports fresh_tomatoes.py to generate the website with bootstrap framework
 import media
 import fresh_tomatoes
 
 
+#Instances of type Movie and TvShow are creted here. For more details, consult media.py. 
 toy_story= media.Movie("Toy Story", 90, "A story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=vwyZH85NQC4") 
 avatar= media.Movie("Avatar", 140, "A marine on an alien planet", "http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 interstellar= media.Movie("Interstellar", 155, "A man travels space in order to save humanity, damn legendary movie", "http://vignette2.wikia.nocookie.net/interstellarfilm/images/4/46/Interstellar_poster.jpg/revision/latest?cb=20140606051928","https://www.youtube.com/watch?v=0vxOhd4qlnA")
@@ -12,18 +20,20 @@ avatar_airbender=media.TvShow("Avatar: the last Airbender",30, "Avatar Ang's adv
 game_of_thrones= media.TvShow("Game of Thrones",40, "he fifth season of the fantasy drama television series Game of Thrones was ordered by HBO in April 2014, together with the sixth season,[1] and premiered on April 12, 2015.","http://upload.wikimedia.org/wikipedia/en/7/71/Game_of_Thrones_S5_Poster.jpg","https://www.youtube.com/watch?v=A0pLbTXPHng",5,5)
 sherlock= media.TvShow("Sherlock",50,"Sherlock is a British crime drama television series and a contemporary adaptation of Sir Arthur Conan Doyle's Sherlock Holmes detective stories.","http://th07.deviantart.net/fs71/PRE/i/2012/155/1/8/sherlock_series_3_fan_poster_2_by_crqsf-d52873p.jpg","https://www.youtube.com/watch?v=9UcR9iKArd0",3,1)
 
+#uncomment below statements to try accessign class variables and methods
 #print(avatar.trailer_youtube_url)
-
 #toy_story.show_trailer()
-
 #interstellar.show_trailer()
-
-
-movies=[toy_story,avatar, interstellar, miss_granny,inglorious_bastards, into_the_wild, avatar_airbender, game_of_thrones, sherlock]
-
-fresh_tomatoes.open_movies_page(movies)
-
 #print(media.Movie.VALID_RATINGS)
 #print(media.Movie.__doc__)
-print(toy_story.duration)
-print(avatar_airbender.season)
+#print(toy_story.duration)
+#print(avatar_airbender.season)
+
+#creates a list containing the instances
+movies=[toy_story,avatar, interstellar, miss_granny,inglorious_bastards, into_the_wild, avatar_airbender, game_of_thrones, sherlock]
+
+#feeds the movie list to fresh_tomatoes.py to create the website
+fresh_tomatoes.open_movies_page(movies)
+
+
+
--- media.py
+++ media.py
@@ -1,15 +1,24 @@
+#!/usr/bin/python
+# filename: media.py
+# description: Part of the movie trailer website project. This script contains class descriptions for 'Video', 'Movie', and 'TvShow
+# author: Xiang Zhao
+
+#need the webbrowser library to open URLs
 import webbrowser
 
+#Class Video is parent for both Movie and TvShow. Contains attributes title and duration
 class Video():
     def __init__(self, title, duration):
         self.title= title
         self.duration= duration
 
 
+#Class Movie inherits from class Video. Contains attributes title, duration, movie_storyline, poster_image, trailer_youtube. Has method show_trailer() to open the youtube trailer. 
 class Movie(Video):
-
+    #this line will be shown when .__doc__ is called
     """ This is class provides a way to store movie related information"""
-
+    
+    #this is an exampler of class variable
     VALID_RATINGS= ["G","PG","PG-13","R"]
 
     def __init__(self, title, duration, movie_storyline, poster_image, trailer_youtube):
@@ -20,12 +29,12 @@ class Movie(Video):
 
         
 
-
+    #this is a class method which will open a URL of the trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
         
         
-        
+#Class TvShow inherits form class Video. Contains attributes title, duration, show_storyline, poster_image, trailer_youtube, season, episode        
 class TvShow(Video):
     def __init__(self, title, duration, show_storyline, poster_image, trailer_youtube, season, episode):
         Video.__init__(self, title, duration)
@@ -34,4 +43,8 @@ class TvShow(Video):
         self.trailer_youtube_url= trailer_youtube
         self.season= season
         self.episode= episode
+        
+    #this is a class method which will open a URL of the trailer    
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
 
